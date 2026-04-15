2427fa3c64bf1bdd3da50b0589f85bfbf3a30d06
ahmed mosalam <ahmedmmosaalam951@gmail.com>
2026-04-15 14:54:03 +0200

    Add files via upload

---
+++ Exploring world layoffs.sql
@@ -0,0 +1,108 @@
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
+
+SELECT MAX(total_laid_off)
+FROM world_layoffs.layoffs_staging2;
+
+SELECT company , percentage_laid_off
+FROM world_layoffs.layoffs_staging2
+WHERE percentage_laid_off = 1
+GROUP BY company
+ORDER BY company ASC;
+
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+
+
+SELECT company, total_laid_off
+FROM world_layoffs.layoffs_staging2
+ORDER BY total_laid_off DESC
+LIMIT 10;
+
+
+SELECT company, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+LIMIT 10;
+
+
+SELECT location, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY location
+ORDER BY 2 DESC;
+
+
+SELECT country, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+
+
+SELECT `date`, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY `date`
+ORDER BY 1 DESC;
+
+
+SELECT YEAR(date), SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY YEAR(date)
+ORDER BY 1 DESC;
+
+
+
+SELECT industry, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+
+SELECT stage, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+
+
+WITH company_year AS
+(
+	SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
+    FROM layoffs_staging2
+    GROUP BY company, YEAR(date)
+)
+, company_year_rank AS (
+	SELECT company, years, total_laid_off, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+    FROM company_year
+)
+SELECT company, years, total_laid_off, ranking
+FROM company_year_rank
+WHERE ranking <= 3
+AND years IS NOT NULL 
+ORDER BY years ASC, total_laid_off DESC;
+
+
+
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC;
+
+
+WITH DATE_CTE AS
+(
+	SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+	FROM layoffs_staging2
+	GROUP BY dates
+	ORDER BY dates ASC
+)
+SELECT dates, SUM(total_laid_off) OVER (ORDER BY dates ASC) as rolling_total_layoffs
+FROM DATE_CTE
+ORDER BY dates ASC;
+
+
+
